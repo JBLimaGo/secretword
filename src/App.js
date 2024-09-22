aa4aa8040fde9dbd2c0e1f8088f1bd50be1056ba
Jefferson Lima <jefferson@araguaiasistemas.com.br>
2024-09-22 17:43:57 -0300

    Implementação de useState, Props.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-
+// CSS
 import "./App.css";
+//React
+import { useCallback, useEffect, useState } from "react";
+
+// data
+import { wordsList } from "./data/words";
+
+// Components
 import StartScreen from "./components/StartScreen";
+import Game from "./components/Game";
+import GameOver from "./components/GameOver";
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <StartScreen />
-    </div>
-  );
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+  const startGame = () => {
+    setGameStage(stages[1].name)
+  }
+
+//  console.log(words);
+  return;
+  <div className="App">
+    
+    {gameStage === "start" && <StartScreen startGame={startGame} />}
+    {gameStage === "game" && <Game />}
+    {gameStage === "end" && <GameOver />}
+  </div>;
 }
 
 export default App;
