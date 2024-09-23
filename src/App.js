594bc04951507a049fb948924b6702b8cd81d1d1
Jefferson Lima <jefferson@araguaiasistemas.com.br>
2024-09-22 22:16:41 -0300

    JSX - Da Pagina do Jogo

--- src/App.js
+++ src/App.js
@@ -21,8 +21,44 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
+  const [pickedWord, setpickedWord] = useState("");
+  const [pickedCategory, setpickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const pickedWordAndCategory = () => {
+    // pick a random category
+    const categories = Object.keys(words);
+    const category =
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    console.log(category);
+
+    // pick a random word
+    const word =
+      words[category][Math.floor(Math.random() * words[category].length)];
+
+    console.log(word);
+
+    return { word, category };
+  };
   // Starts the secret word game
   const startGame = () => {
+    // pick word and pick category
+    const { word, category } = pickedWordAndCategory();
+
+    // Creat an array of letters
+    let wordLetters = word.split("");
+
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+    console.log(word, category);
+    console.log(wordLetters);
+
+    // fill states
+    setpickedWord(word);
+    setpickedCategory(category);
+    setLetters(letters);
+
     setGameStage(stages[1].name);
   };
 
@@ -36,6 +72,7 @@ function App() {
     setGameStage(stages[0].name);
   };
   // console.log(words);
+
   return (
     <div className="App">
       {gameStage === "start" && <StartScreen startGame={startGame} />}
